a6a9eb76e11515bcbd7661c285d00e96618a20c8
killian31 <80256683+killian31@users.noreply.github.com>
2024-02-09 15:47:56 +0100

    feat: inference on cart

--- cart.py
+++ cart.py
@@ -1,25 +1,25 @@
+import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, plot_tree
-import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score
+
+from clustering import add_pca_features, create_clustered_df
 from pca_embed import add_pca_features
 
-#things needed to add clusters as info
-from utils import get_random_elements_from_cluster, create_clustered_col
-from clustering import create_clustered_df, add_pca_features
-
-
+# things needed to add clusters as info
+from utils import create_clustered_col, get_random_elements_from_cluster
 
 
 def cart(pca_df, plotting=False, random_state=29):
 
-    X = pca_df.loc[:, pca_df.columns != 'genre']  # Features
-    y = pca_df['genre']  # Target variable
-
+    X = pca_df.loc[:, pca_df.columns != "genre"]  # Features
+    y = pca_df["genre"]  # Target variable
 
     # Splitting the dataset into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=random_state
+    )
 
     # Creating Decision Tree classifier object
     classifier = DecisionTreeClassifier(max_depth=8)
@@ -34,20 +34,26 @@ def cart(pca_df, plotting=False, random_state=29):
     accuracy = accuracy_score(y_test, y_pred)
     print("Accuracy:", accuracy)
 
-    if ploting==True:
+    if plotting == True:
         # Visualizing the Decision Tree
-        plt.figure(figsize=(20,10))
-        plot_tree(classifier, filled=True, feature_names=X.columns, class_names=classifier.classes_)
+        plt.figure(figsize=(20, 10))
+        plot_tree(
+            classifier,
+            filled=True,
+            feature_names=X.columns,
+            class_names=classifier.classes_,
+        )
         plt.show()
-    return(classifier)
+    return classifier
+
 
 if __name__ == "__main__":
     df = pd.read_csv("data/full_data_embed.csv")
-    
+
     pca_df, _ = add_pca_features(df, 37)
 
-    #adding the model appartenance
+    # adding the model appartenance
     full_df = create_clustered_df(pca_df, n_clusters=8)
     cluster_column = create_clustered_col(full_df, pca_df)
 
-    model=cart(full_df, False, random_state=29)
+    model = cart(full_df, False, random_state=29)
